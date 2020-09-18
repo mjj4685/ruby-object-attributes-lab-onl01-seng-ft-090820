46d3d6b30d2798677192fbbd3f555b79d31b1e73
MJ Johnson <mjohnson4685@gmail.com>
2020-09-18 00:06:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,24 @@
 class Person
-
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+ 
+  def initialize(name)
+    @name = name
   end
-
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
-  end
-
+ 
   def name
-    "#{@first_name} #{@last_name}".strip
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
   end
 
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+ 
 end
